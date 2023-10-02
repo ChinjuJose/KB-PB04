ed3869bc77762e67d803a90375a39a3e65ddf912
ChinjuJose <73712484+ChinjuJose@users.noreply.github.com>
2023-10-02 04:58:04 +0400

    Add files via upload

---
+++ TA1.py
@@ -0,0 +1,16 @@
+name = "Ria"
+
+if name=="Ria":
+    print("Good Morning")
+
+
+
+number = int(input("Enter the number:"))
+
+if number%2 == 0:
+    print(str(number)+" is an even number")
+else:
+    print(str(number)+" is an odd number")
+
+
+    
